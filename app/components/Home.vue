d9361e730b71b0b27dbaee617ecdb843504edde5
yosecc <yosec.cervino@gmail.com>
2023-11-01 20:36:21 -0300

    home data

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,9 +1,8 @@
 <template>
   <Page actionBarHidden="true" >
-    <GridLayout rows="auto,*" class="coverImage">
+    <GridLayout rows="auto,*" v-if="Object.entries(homeView).length" class="coverImage">
 
       <GridLayout row="0" padding="10">
-        <!-- <Label :text="isDark" /> -->
         <Image src="res://logo_white" v-if="isDark" width="100" paddingTop="10" />
         <Image src="res://logo_black" v-else width="100" paddingTop="10" />
       </GridLayout>
@@ -13,57 +12,81 @@
         <StackLayout row="0" paddingBottom="10">
           <Label textAlignment="center" class="title" padding="0" margin="0" lineHeight="0" textWrap="true">
             <FormattedString  >
-              <Span text="VIAJA" fontSize="60"  fontWeight="800" style="line-height: 0px;" />
-              <Span text=" sin " fontSize="36"  fontWeight="700" style="color: #E74117;line-height: 0px;" />
-              <Span text="LIMITES" fontSize="60" fontWeight="800" :marginTop="-10" style="line-height: 0px;margin-top:-10" />
+              <Span 
+                v-for="(item, key) in homeView.header.title"
+                :key="`title-${key}`"
+                :text="item.text"
+                :fontSize="item.fontSize"
+                :fontWeight="item.fontWeight"
+                :color="item.color"
+                style="line-height: 0px;" 
+              />
             </FormattedString>
           </Label>
-          <Label text="Encuentra y alquila en sencillos pasos" class="title" textAlignment="center" margin="0" padding="0" fontSize="14" fontWeight="400" />
+          <Label 
+            :text="homeView.header.sub.text" 
+            :textAlignment="homeView.header.sub.textAlignment"
+            :fontSize="homeView.header.sub.fontSize"
+            :fontWeight="homeView.header.sub.fontWeight"
+            :color="homeView.header.sub.color"
+            class="title" 
+            margin="0" 
+            padding="0" 
+          />
         </StackLayout>
         <!--  -->
         <StackLayout row="1" padding="10 5 5 5">
-          <StackLayout class="card" padding="20" background="#E74117" >
-            <label textAlignment="center" color="white" fontWeight="900" @tap="logMessage" fontSize="20" text="INICIA TU RESERVA" />
+          <StackLayout class="card" padding="20" :background="homeView.body.button.background" >
+            <label 
+              :textAlignment="homeView.body.button.textAlignment"
+              :color="homeView.body.button.color"
+              :fontWeight="homeView.body.button.fontWeight"
+              :fontSize="homeView.body.button.fontSize"
+              :text="homeView.body.button.text" 
+              @tap="logMessage" 
+            />
           </StackLayout>
         </StackLayout>
         <!--  -->
         <WrapLayout row="2" >
-          <StackLayout width="50%" height="50%" padding="5">
-
-            <FlexboxLayout  class="card"  background="" width="100%" height="100%" flexDirection="column" justifyContent="center" alignItems="center" >
-              <Image src="res://car" marginBottom="10" height="40" width="40" row="0" />
-              <label textAlignment="center" text="Flota" fontSize="20" fontWeight="400" textWrap />
-            </FlexboxLayout>
-          </StackLayout>
-          <StackLayout width="50%" height="50%" padding="5">
-            <FlexboxLayout class="card"  background=""  width="100%" height="100%" flexDirection="column" justifyContent="center" alignItems="center" >
-              <Image src="res://edificio" marginBottom="10" height="40" width="40" row="0" />
-              <label textAlignment="center" text="Oficina" fontSize="20" fontWeight="400" textWrap />
-            </FlexboxLayout>
-          </StackLayout>
-          <StackLayout width="50%" height="50%" padding="5">
-            <FlexboxLayout  class="card" background=""  width="100%" height="100%" flexDirection="column" justifyContent="center" alignItems="center" >
-              <Image src="res://reserve" marginBottom="10" height="40" width="40" row="0" />
-              <label textAlignment="center" text="Mis reservas" fontSize="20" fontWeight="400" textWrap />
-            </FlexboxLayout>
-          </StackLayout>
-          <StackLayout width="50%" height="50%" padding="5">
+          <StackLayout 
+            v-for="(item, key) in homeView.body.gridButtons"
+            :key="`card-${key}`"
+            :width="item.box.width" 
+            :height="item.box.height" 
+            padding="5"
+          >
             <FlexboxLayout class="card"  background="" width="100%" height="100%" flexDirection="column" justifyContent="center" alignItems="center" >
-              <Image src="res://person" marginBottom="10" height="40" width="40" row="0" />
-              <label textAlignment="center" text="Perfil" fontSize="20" fontWeight="400" textWrap />
-
+              <Image 
+                marginBottom="10" 
+                :src="item.src" 
+                :height="item.height" 
+                :width="item.width" 
+              />
+              <label 
+                :textAlignment="item.textAlignment" 
+                :text="item.text" 
+                :fontSize="item.fontSize" 
+                :fontWeight="item.fontWeight" 
+                textWrap 
+              />
             </FlexboxLayout>
           </StackLayout>
         </WrapLayout>
         <!--  -->
         <StackLayout row="3" padding="5 5 5 5" background="">
           <GridLayout row="3" columns="*" class="card" background="" padding="0" height="100%" >
-            <Image src="~/assets/furgo.png" width="100%" row="0" borderRadius="20" stretch="aspectFit" />
+            <Image :src="homeView.body.banner.src" width="100%" row="0" borderRadius="20" stretch="aspectFit" />
           </GridLayout>
         </StackLayout>
         <!--  -->
         <FlexboxLayout width="100%" row="4" columns="*,*,*" padding="0 0 10 0">
-          <FlexboxLayout padding="5" width="33.33%" col="0">
+          <FlexboxLayout
+            v-for="(item, key) in homeView.body.gridButtonsBottom"
+            :key="`cardbottom-${key}`"
+            padding="5" 
+            :width="item.box.width" 
+          >
             <FlexboxLayout 
               padding="19" 
               width="100%" 
@@ -73,22 +96,21 @@
               justifyContent="center" 
               alignItems="center" 
             >
-              <Image src="res://phone" marginBottom="10" height="25" width="25" row="0" />
-              <label textAlignment="center" text="Contacto" fontSize="12" fontWeight="200" textWrap />
+              <Image 
+                :src="item.src"
+                :height="item.height" 
+                :width="item.width" 
+                marginBottom="10" 
+              />
+              <label 
+                :textAlignment="item.textAlignment"
+                :text="item.text"
+                :fontSize="item.fontSize"
+                :fontWeight="item.fontWeight"
+                textWrap 
+              />
             </FlexboxLayout>
-          </FlexboxLayout>
-          <FlexboxLayout padding="5" width="33.33%" col="1">
-            <FlexboxLayout padding="19" class="card" background="" width="100%" height="100%" flexDirection="column" justifyContent="center" alignItems="center" >
-              <Image src="res://card" marginBottom="10" height="25" width="25" row="0" />
-              <label textAlignment="center" text="Metodo de pago" fontSize="12" fontWeight="200" textWrap />
-            </FlexboxLayout>
-          </FlexboxLayout>
-          <FlexboxLayout padding="5" width="33.33%" col="2">
-            <FlexboxLayout padding="19" class="card" background="" width="100%" height="100%" flexDirection="column" justifyContent="center" alignItems="center" >
-              <Image src="res://interrogative" marginBottom="10" height="25" width="25" row="0" />
-              <label textAlignment="center" text="Preguntas Frecuentes" fontSize="12" fontWeight="200" textWrap />
-            </FlexboxLayout>
-          </FlexboxLayout>
+          </FlexboxLayout> 
         </FlexboxLayout>
       </GridLayout>
 
@@ -98,30 +120,33 @@
 
 <script lang="ts">
   import Vue from "nativescript-vue";
-  
-  // import { android as androidApp, ios as iosApp } from "@nativescript/core/application";
   import { Application, Color, Utils } from '@nativescript/core'
-  import { } from '@nativescript/core/ui/'
-  import { device } from "@nativescript/core/platform";
-  // import Theme from "@nativescript/theme";
-  // import android from "@nativescript/android/framework/";
+  import { home } from '~/data/home'
   
   export default Vue.extend({
+    data(){
+      return{
+        homeView: {},
+      }
+    },
     computed: {
       message() {
         return "Blank {N}-Vue app";
       },
       isDark(){
-        // console.log( Application.systemAppearance())
         return Application.systemAppearance() === 'dark'
       }
     },
+    created(){
+      this.homeView = home
+    },
     mounted(){
+      console.log(this.homeView)
 
     },
     methods: {
       logMessage() {
-        console.log()
+        console.log(this.homeView)
 
       }
     }
--- app/data/home.ts
+++ app/data/home.ts
@@ -1,1 +1,142 @@
-export const = 
+export const home = {
+    idioma: 'ES',
+    header: {
+        title: [
+            {
+                text: 'VIAJA',
+                fontSize: 60, 
+                fontWeight:"800",
+                color: '',
+            },
+            {
+                text: ' sin ',
+                fontSize: 36,
+                fontWeight:"700",
+                color: '#E74117'   
+            },
+            {
+                text: 'LIMITES',
+                fontSize: 60, 
+                fontWeight:"800",
+                color: ''
+
+            },
+        ],
+        sub: {
+            text: 'Encuentra y alquila en sencillos pasos',
+            fontSize: 14, 
+            fontWeight:"400",
+            color: '',
+            textAlignment:"center"
+        }
+    },
+    body:{
+        button:{
+            text:"INICIA TU RESERVA",
+            textAlignment:"center" , 
+            color:"white" , 
+            fontWeight:"900", 
+            fontSize:"20",
+            background: '#E74117'
+        },
+        gridButtons: [
+            {
+                textAlignment:"center",
+                text:"Flota" ,
+                fontSize:"20" ,
+                fontWeight:"400" ,
+                src:"res://car",
+                height:"40" ,
+                width:"40",
+                box:{
+                    width: '50%',
+                    height: '50%'
+                }
+            },
+            {
+                textAlignment:"center",
+                text:"Oficina" ,
+                fontSize:"20" ,
+                fontWeight:"400" ,
+                src:"res://edificio",
+                height:"40" ,
+                width:"40",
+                box:{
+                    width: '50%',
+                    height: '50%'
+                }
+            },
+            {
+                textAlignment:"center",
+                text:"Mis reservas" ,
+                fontSize:"20" ,
+                fontWeight:"400" ,
+                src:"res://reserve",
+                height:"40" ,
+                width:"40",
+                box:{
+                    width: '50%',
+                    height: '50%'
+                }
+            },
+            {
+                textAlignment:"center",
+                text:"Perfil" ,
+                fontSize:"20" ,
+                fontWeight:"400" ,
+                src:"res://person",
+                height:"40" ,
+                width:"40",
+                box:{
+                    width: '50%',
+                    height: '50%'
+                }
+            },
+        ],
+        banner: {
+            src: "~/assets/furgo.png"
+        },
+        gridButtonsBottom:[
+            {
+                textAlignment:"center",
+                text:"Contacto" ,
+                fontSize:"12" ,
+                fontWeight:"200" ,
+                src:"res://phone",
+                height:"25" ,
+                width:"25",
+                box:{
+                    width: '33.33%',
+                    height: '33.33%'
+                }
+            },
+            {
+                textAlignment:"center",
+                text:"Metodo de pago" ,
+                fontSize:"12" ,
+                fontWeight:"200" ,
+                src:"res://card",
+                height:"25" ,
+                width:"25",
+                box:{
+                    width: '33.33%',
+                    height: '33.33%'
+                }
+            },
+            {
+                textAlignment:"center",
+                text:"Preguntas Frecuentes" ,
+                fontSize:"12" ,
+                fontWeight:"200" ,
+                src:"res://interrogative",
+                height:"25" ,
+                width:"25",
+                box:{
+                    width: '33.33%',
+                    height: '33.33%'
+                }
+            },
+            
+        ]
+    }
+}
