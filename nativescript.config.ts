ece76a221f2c7869a991c84e4d045dc28e3d1dbe
yosecc <yosec.cervino@gmail.com>
2023-11-02 21:30:18 -0300

    iconos ios

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -1,7 +1,7 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-  id: 'org.nativescript.app',
+  id: 'es.123renting.app',
   appPath: 'app',
   appResourcesPath: 'App_Resources',
   android: {
