e6f3a85e9d3d4f4dbb52552201b917a853bbc98c
yosecc <yosec.cervino@gmail.com>
2023-11-14 20:34:38 -0300

    cofign name

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -1,7 +1,7 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-  id: 'es.123renting.app',
+  id: 'org.nativescript.app',
   appPath: 'app',
   appResourcesPath: 'App_Resources',
   android: {
